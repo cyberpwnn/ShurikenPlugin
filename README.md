328aad594f78628af324452ffd2fe20f8ed051bb
Daniel Mills <danielmillst@gmail.com>
2020-12-15 16:37:37 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-ShurikenPlugin
+# ShurikenPlugin
+A great start to a bloated but capable plugin.
+
+#### To rename this template please do the following simple steps
+
+1. Update the properties in build.gradle in the designated section
+2. In settings.gradle, change the name from "APlugin" to your project name
+3. Use SHIFT+F6 on the package pkg (in src/java/main) to your package
+4. Use SHIFT+F6 on the class APlugin (in src/main/java) to your project name
+5. Update the getTag() function in the main plugin class to your project name
+6. Invalidate Caches/Restart (Because JetBricks RetardiJ cache eviction sucks)
+ 
--- src/main/java/pkg/APlugin.java
+++ src/main/java/pkg/APlugin.java
@@ -1,5 +1,27 @@
 package pkg;
 
-public class ShurikenPlugin {
+import ninja.bytecode.shuriken.bukkit.bukkit.plugin.Instance;
+import ninja.bytecode.shuriken.bukkit.bukkit.plugin.ShurikenPlugin;
+import ninja.bytecode.shuriken.bukkit.util.text.C;
+import ninja.bytecode.shuriken.bukkit.util.text.TXT;
 
+public class APlugin extends ShurikenPlugin {
+    @Instance
+    public static APlugin instance;
+
+    @Override
+    public void start()
+    {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public String getTag(String subTag) {
+        return TXT.makeTag(C.DARK_GRAY, C.BLUE, C.DARK_GRAY, C.GRAY, "APlugin");
+    }
 }
